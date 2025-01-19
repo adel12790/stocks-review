d412730974affb3648a5de05f3a3381e50497f1b
Mo Adel <adel12790@gmail.com>
2025-01-19 12:13:20 +0100

    init

---
+++ api/Interfaces/IStockRepository.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Dtos.Stock;
+using api.Models;
+
+namespace api.Interfaces
+{
+    public interface IStockRepository
+    {
+        Task<List<Stock>> GetAllAsync();
+        Task<Stock?> GetById(int id); // first or default can be null so we added the ?
+        Task<Stock> CreateAsync(Stock stockModel);
+        Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockModel);
+        Task<Stock?> DeleteAsync(int id);
+    }
+}
---
+++ api/Mappers/StockMappers.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Dtos.Stock;
+using api.Models;
+
+namespace api.Mappers
+{
+    public static class StockMappers
+    {
+        public static StockDto toStockDto(this Stock stockModel)
+        {
+            return new StockDto
+            {
+                Id = stockModel.Id,
+                Symbol = stockModel.Symbol,
+                CompanyName = stockModel.CompanyName,
+                Purchase = stockModel.Purchase,
+                LastDiv = stockModel.LastDiv,
+                Industry = stockModel.Industry,
+                MarketCap = stockModel.MarketCap
+            };
+        }
+
+        public static Stock ToStockFromCreateDto(this CreateStockRequestDto createDto)
+        {
+            return new Stock
+            {
+                Symbol = createDto.Symbol,
+                CompanyName = createDto.CompanyName,
+                Purchase = createDto.Purchase,
+                LastDiv = createDto.LastDiv,
+                Industry = createDto.Industry,
+                MarketCap = createDto.MarketCap
+            };
+        }
+    }
+}
