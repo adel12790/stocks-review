f70c7984b9ca83a5438fbd32dc22fce9ef09614d
Mo Adel <adel12790@gmail.com>
2025-01-21 06:57:30 +0100

    Sorting in place

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -32,6 +32,12 @@ namespace api.Repository
                 stocks = stocks.Where(x => x.Symbol.Contains(query.Symbol));
             }
 
+            if(!string.IsNullOrWhiteSpace(query.SortBy)){
+                if(query.SortBy.Equals("Symbol", StringComparison.OrdinalIgnoreCase)){
+                    stocks = query.IsDecending ? stocks.OrderByDescending(s => s.Symbol) : stocks.OrderBy(s => s.Symbol);
+                }
+            }
+
             return await stocks.ToListAsync();
         }
 
